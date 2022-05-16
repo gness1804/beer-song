099ac7874109397e8caa55a4146b34831ca9d94e
gness1804 <graham.nessler@gmail.com>
2022-05-16 09:44:19 -0500

    first test passing

--- beer-song.ts
+++ beer-song.ts
@@ -1,5 +1,7 @@
-export function verse(index: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function verse(index: number): string {
+  return `${index} bottles of beer on the wall, ${index} bottles of beer.
+Take one down and pass it around, ${index  - 1 } bottles of beer on the wall.
+`;
 }
 
 export function sing(
