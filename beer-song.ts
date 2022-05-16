d713365028a522c9f8fd01c92d804b5788b17737
gness1804 <graham.nessler@gmail.com>
2022-05-16 09:54:23 -0500

    add 0 and 1 index cases

--- beer-song.ts
+++ beer-song.ts
@@ -1,4 +1,10 @@
 export function verse(index: number): string {
+  if (index === 1) return `1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+`
+if (index === 0) return `No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+`
   return `${index} bottles of beer on the wall, ${index} bottles of beer.
 Take one down and pass it around, ${index  - 1 } bottles of beer on the wall.
 `;
